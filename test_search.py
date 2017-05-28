07f71dd4ce4d8985ac91f24f50a15804707e0467
James Cuénod <j3frea@gmail.com>
2017-05-27 22:42:46 -0500

    Significant changes: added support for filtering, builds results better

--- test_search.py
+++ test_search.py
@@ -2,47 +2,6 @@ import sqlite3
 dbfile = "textfabric.sqlite"
 db = sqlite3.connect(dbfile)
 
-# search_data = {"query":[{"lex":"ZNH[","vs":"qal"},{"lex":"<L"}],"search_range":"clause"}
-# search_data = {"query":[{"lex":"XZQ[","vs":"qal"},{"lex":"B"}],"search_range":"sentence"}
-# search_data = {"query":[{"lex":"R>H[","vs":"qal"},{"gn":"f"}],"search_range":"clause"}
-# search_data = {"query":[{"lex":"R>H[","vs":"qal"},{"gn":"f"},{"lex":"H"}],"search_range":"clause"}
-# search_data = {"query":[{"gn":"f"},{"lex":"H"},{"vs":"qal","voc_utf8":"חזק"}],"search_range":"clause"}
-search_data = {"query":[{"tricons":"משׁה"},{"lex":">MR["},{"voc_utf8":"אֶל","invert":"t"}],"search_range":"clause"}
-
-query = search_data["query"]
-search_range = search_data["search_range"]
-
-word_groups_to_exclude = []
-word_group_with_match = [[] for i in range(len(query))]
-found_words = []
-
-results = []
-results_to_exclude = []
-for q in query:
-	invert = False
-	if "invert" in q:
-		invert = q["invert"] == "t"
-		del q["invert"]
-	query_keys = [ k for k in q ]
-	requirementArray = list(map(lambda k: '`{}`="{}"'.format(k, q[k]), query_keys))
-	print(requirementArray)
-	sql_query = "SELECT `" + search_range + "` FROM `TreeData`, `WordData` WHERE `WordData`.`wid` = `TreeData`.`wid` AND " + " AND ".join(requirementArray)
-	print("QUERY:",sql_query)
-	db_cursor = db.execute(sql_query)
-	temp_results = list(map(lambda x: x[0], db_cursor.fetchall()))
-	if invert:
-		results_to_exclude += temp_results
-	else:
-		results.append(temp_results)
-	print("RESULTS:", len(temp_results))
-
-# intersected_set = list(set.intersection(*map(set, results)))
-intersection_to_filter = set(results[0]).intersection(*results[1:])
-intersected_set = list(filter(lambda x: x not in results_to_exclude, intersection_to_filter))
-# print(r)
-print("\nMATCHES:", len(intersected_set))
-
-
 book_to_index = {
 	"Genesis": 1,
 	"Exodus": 2,
@@ -84,6 +43,57 @@ book_to_index = {
 	"Zechariah": 38,
 	"Malachi": 39,
 }
+
+# search_data = {"query":[{"lex":"ZNH[","vs":"qal"},{"lex":"<L"}],"search_range":"clause"}
+# search_data = {"query":[{"lex":"XZQ[","vs":"qal"},{"lex":"B"}],"search_range":"sentence"}
+# search_data = {"query":[{"lex":"R>H[","vs":"qal"},{"gn":"f"}],"search_range":"clause"}
+# search_data = {"query":[{"lex":"R>H[","vs":"qal"},{"gn":"f"},{"lex":"H"}],"search_range":"clause"}
+# search_data = {"query":[{"gn":"f"},{"lex":"H"},{"vs":"qal","voc_utf8":"חזק"}],"search_range":"clause"}
+# search_data = {"query":[{"tricons":"משׁה"},{"lex":">MR["},{"voc_utf8":"אֶל","invert":"t"}],"search_range":"clause"}
+search_data = {
+	"query":[{"lex":"MYRJM/"}],
+	"search_range":"sentence",
+	"search_filter":["Deuteronomy"]
+}
+
+query = search_data["query"]
+search_range = search_data["search_range"]
+search_filter = None if "search_filter" not in search_data else search_data["search_filter"]
+
+word_groups_to_exclude = []
+word_group_with_match = [[] for i in range(len(query))]
+found_words = []
+
+results = []
+results_to_exclude = []
+for q in query:
+	invert = False
+	if "invert" in q:
+		invert = q["invert"] == "t"
+		del q["invert"]
+	query_keys = [ k for k in q ]
+	requirementArray = list(map(lambda k: '`{}`="{}"'.format(k, q[k]), query_keys))
+	print(requirementArray)
+	filter_requirement = " AND `TreeData`.`book` IN ('" + "','".join(map(lambda s: str(book_to_index[s]), search_filter)) + "')" if search_filter is not None else ""
+	sql_query = "SELECT `" + search_range + "` FROM `TreeData`, `WordData` WHERE `WordData`.`wid` = `TreeData`.`wid` AND " + " AND ".join(requirementArray) + filter_requirement
+	print("QUERY:",sql_query)
+	db_cursor = db.execute(sql_query)
+	temp_results = list(map(lambda x: x[0], db_cursor.fetchall()))
+	if invert:
+		results_to_exclude += temp_results
+	else:
+		results.append(temp_results)
+	print("RESULTS:", len(temp_results))
+
+# intersected_set = list(set.intersection(*map(set, results)))
+intersection_to_filter = set(results[0]).intersection(*results[1:])
+intersected_set = list(filter(lambda x: x not in results_to_exclude, intersection_to_filter))
+# print(r)
+print("\nMATCHES:", len(intersected_set))
+
+
+
+
 def keyFromValue(dictionary, value):
 	return list(dictionary.keys())[list(dictionary.values()).index(value)]
 def indexToPassage(rid):
@@ -92,26 +102,50 @@ def indexToPassage(rid):
 	book_index = int((rid - chapter_number - verse_number) / 1000000)
 	return (keyFromValue(book_to_index, book_index), chapter_number, verse_number)
 
-sql_query = "SELECT DISTINCT `rid`, `{}` FROM `TreeData` WHERE `{}` IN ({})".format(search_range, search_range, ",".join(map(str, intersected_set)))
+
+## Pair up verses with range_matches
+verse_range_per_range_match = []
+sql_query = "SELECT DISTINCT `rid`, `{range}` FROM `TreeData` WHERE `{range}` IN ({range_matches})".format(range=search_range, range_matches=",".join(map(str, intersected_set)))
+db_cursor = db.execute(sql_query)
+rid_for_search_matches = db_cursor.fetchall()
+for range_match in intersected_set:
+	filtered_results = filter(lambda r: r[1] == range_match, rid_for_search_matches)
+	these_rids = list(map(lambda fr: fr[0], filtered_results))
+	minV = min(these_rids)
+	maxV = max(these_rids)
+	reference = "{} {}:{}".format(*indexToPassage(minV))
+
+	if minV is not maxV:
+		reference += "-" + str(indexToPassage(maxV)[2])
+	verse_range_per_range_match.append({
+		"node_matched": range_match,
+		"rids": these_rids,
+		"reference": reference,
+		"order_key": minV
+	})
+
+
+## Fill verse_range_per_range_match with words
+rids_to_match = list(map(lambda r: r[0], rid_for_search_matches))
+sql_query = "SELECT DISTINCT `rid`, `{range}`, `g_word_utf8`, `trailer_utf8` FROM `WordData`, `TreeData` WHERE `TreeData`.`wid` = `WordData`.`wid` AND `TreeData`.`rid` IN ({rids})".format(range=search_range, rids=",".join(map(str, rids_to_match)))
 db_cursor = db.execute(sql_query)
 rid_and_range_matches = db_cursor.fetchall()
-
-def wordsForOtypeIn(otype, nodes):
-	sql_query = "SELECT `rid`, `g_word_utf8`, `trailer_utf8` FROM `WordData`, `TreeData` WHERE `TreeData`.`wid` = `WordData`.`wid` AND `TreeData`.`{range}` IN ({range_matches}) ORDER BY `WordData`.`wid`".format(range=otype, range_matches=",".join(map(str, nodes)))
-	print(sql_query)
-	db_cursor = db.execute(sql_query)
-	return db_cursor.fetchall()
-
-currentRid = 0
-if True: # Print results
-	for row in wordsForOtypeIn(search_range, map(lambda x: x[1], rid_and_range_matches)):
-		if currentRid != row[0]:
-			currentRid = row[0]
-			print("\n\n", indexToPassage(currentRid))
-		if row[1]:
-			print(row[1], end="")
+for i, verse_match in enumerate(verse_range_per_range_match):
+	rows_for_match = list(filter(lambda r: r[0] in verse_match["rids"], rid_and_range_matches))
+	currentRid = 0
+	verse_text = ""
+	reference = None
+	for row in rows_for_match:
+		# This is where we start trying to do highlighting stuff...
 		if row[2]:
-			print(row[2], end="")
+			verse_text += row[2]
+		if row[3]:
+			verse_text += row[3]
+	verse_range_per_range_match[i]["verse_text"] = verse_text
+
+if True:
+for v in sorted(verse_range_per_range_match, key=lambda k: k['order_key']) :
+	print(v["reference"],"\n",v["verse_text"],"\n")
 
 print("\ndone")
 
